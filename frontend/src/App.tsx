6262b17f3d64a1aa5ae1d08631be3e32e2ea442a
fm454 <fm454@cornell.edu>
2025-07-24 23:57:40 -0400

    project file setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-500">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1 className="text-3xl font-bold underline">Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        </div>
+  )
+}
+
+export default App
